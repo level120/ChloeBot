c5e1be7b3f1ce9d1c6f92a2f8fd1c9e67fcb136b
Gusam Park <tnara9573@gmail.com>
2019-09-29 22:56:33 +0900

    feat: 시작 스크립트에 최신 파일 가져오는 기능 추가

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 ﻿#!/bin/sh
 
+cd /GameBot/Core
+git fetch && git pull origin master
+
 chmod 755 /GameBot/Core/Core/bin/Release/netcoreapp3.0/ubuntu.18.04-x64/ChloeBot
 /GameBot/Core/Core/bin/Release/netcoreapp3.0/ubuntu.18.04-x64/ChloeBot
