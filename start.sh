a6b8df04013437ddcf445f0951196329b1655036
Gusam Park <tnara9573@gmail.com>
2019-09-30 20:08:14 +0900

    feat: docker 컨테이너에서 재실행 시 자동으로 빌드 후 bot 실행하는 기능

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
-﻿#!/bin/sh
+﻿#!/bin/bash
 
+# Pulling files
 cd /GameBot/Core
 git fetch && git pull origin master
 
-chmod 755 /GameBot/Core/Core/bin/Release/netcoreapp3.0/ubuntu.18.04-x64/ChloeBot
-/GameBot/Core/Core/bin/Release/netcoreapp3.0/ubuntu.18.04-x64/ChloeBot
+# Build project for docnet core
+dotnet clean
+dotnet build -c Release -r ubuntu.18.04-x64
+
+# Change permission to runable
+cd /GameBot/Core/Core/bin/Release/netcoreapp3.0/ubuntu.18.04-x64
+chmod 755 ChloeBot
+
+# Run discord bot
+./ChloeBot
